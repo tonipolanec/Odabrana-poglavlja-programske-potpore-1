7b387e7934eba896a55415b7d59d29b1291e8961
Toni Polanec <toni.polanec@gmail.com>
2022-12-01 15:12:49 +0100

    Fixed JUnit tests on hw04

--- hw04-0036535771/src/test/java/hr/fer/oprpp1/hw04/db/QueryParserTest.java
+++ hw04-0036535771/src/test/java/hr/fer/oprpp1/hw04/db/QueryParserTest.java
@@ -10,37 +10,37 @@ public class QueryParserTest {
 	
 	@Test
 	public void testSize() {
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName"));
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName ="));
-		assertDoesNotThrow(() -> new QueryParser(" firstName = \"Toni\""));
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName = \"Toni\" AND"));
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName = \"Toni\" AND lastName"));
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName = \"Toni\" AND lastName ="));
-		assertDoesNotThrow(() -> new QueryParser(" firstName = \"Toni\" AND lastName = \"Polanec\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName"));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName ="));
+		assertDoesNotThrow(() -> new QueryParser("query firstName = \"Toni\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName = \"Toni\" AND"));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName = \"Toni\" AND lastName"));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName = \"Toni\" AND lastName ="));
+		assertDoesNotThrow(() -> new QueryParser("query firstName = \"Toni\" AND lastName = \"Polanec\""));
 	}
 	
 	@Test
 	public void testValidAttributes() {
-		assertDoesNotThrow(() -> new QueryParser(" jmbag=\"string\""));
-		assertDoesNotThrow(() -> new QueryParser(" firstName=\"string\""));
-		assertDoesNotThrow(() -> new QueryParser(" lastName=\"string\""));
+		assertDoesNotThrow(() -> new QueryParser("query jmbag=\"string\""));
+		assertDoesNotThrow(() -> new QueryParser("query firstName=\"string\""));
+		assertDoesNotThrow(() -> new QueryParser("query lastName=\"string\""));
 	}
 	@Test
 	public void testInvalidAttributes() {
-		assertThrows(ParserException.class, () -> new QueryParser(" attribute=\"string\""));
-		assertThrows(ParserException.class, () -> new QueryParser(" jmbbbbag=\"string\""));
-		assertThrows(ParserException.class, () -> new QueryParser(" first_name=\"string\""));
-		assertThrows(ParserException.class, () -> new QueryParser(" lAstNaMe=\"string\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query attribute=\"string\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query jmbbbbag=\"string\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query first_name=\"string\""));
+		assertThrows(ParserException.class, () -> new QueryParser("query lAstNaMe=\"string\""));
 	}
 	
 	@Test
 	public void testInvalidSeparation() {
-		assertThrows(ParserException.class, () -> new QueryParser(" firstName = \"Toni\" lastName = \"Polanec\" ="));
+		assertThrows(ParserException.class, () -> new QueryParser("query firstName = \"Toni\" lastName = \"Polanec\" ="));
 	}
 	
 	@Test
 	public void testDemo1() {
-		qp = new QueryParser(" jmbag \t =\"0123456789\" ");
+		qp = new QueryParser("query jmbag \t =\"0123456789\" ");
 		
 		assertTrue(qp.isDirectQuery());
 		assertEquals("0123456789", qp.getQueriedJMBAG());
@@ -49,7 +49,7 @@ public class QueryParserTest {
 	
 	@Test
 	public void testDemo2() {
-		qp = new QueryParser("jmbag=\"0123456789\" and lastName>\"J\"");
+		qp = new QueryParser("query jmbag=\"0123456789\" and lastName>\"J\"");
 		
 		assertFalse(qp.isDirectQuery());
 		assertThrows(IllegalStateException.class, () -> qp.getQueriedJMBAG());
