63409a752bc769cdf26e80c1d21a0cd49f490352
Toni Polanec <toni.polanec@gmail.com>
2022-12-06 19:00:23 +0100

    Initial hw05 commit

---
+++ hw05-0036535771/src/main/java/hr/fer/oprpp1/hw05/crypto/Crypto.java
@@ -0,0 +1,14 @@
+package hr.fer.oprpp1.hw05.crypto;
+
+public class Crypto {
+
+	public Crypto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
