5bfe760743cd788aa6feb276630705fadec075b4
Toni Polanec <toni.polanec@gmail.com>
2022-12-13 10:42:55 +0100

    06: Initial commit
    Fixed warning from hw02
    Added fractal-viewer-1.0.jar to local maven repository

--- hw02-0036536771/src/main/java/hr/fer/oprpp1/custom/collections/demo/Demo.java
+++ hw02-0036536771/src/main/java/hr/fer/oprpp1/custom/collections/demo/Demo.java
@@ -196,28 +196,28 @@ public class Demo {
 //      ---------------------------------------------------------------------------	
 //		Subtask 6
 		
-//		//List col1 = new ArrayIndexedCollection();
-//		//List col2 = new LinkedListIndexedCollection();
-//		List col1 = new LinkedListIndexedCollection();
-//		List col2 = new ArrayIndexedCollection();
-//		
-//		col1.add("Ivana");
-//		col2.add("Jasna");
-//		
-//		Collection col3 = col1;
-//		Collection col4 = col2;
-//		
-//		col1.get(0);
-//		col2.get(0);
-//	
-//		//col3.get(0); // neće se prevesti! Razumijete li zašto? 
-//		//col4.get(0); // neće se prevesti! Razumijete li zašto?
-//		// Class Collection nema metodu get!
-//	
-//		col1.forEach(System.out::println); // Ivana
-//		col2.forEach(System.out::println); // Jasna
-//		col3.forEach(System.out::println); // Ivana
-//		col4.forEach(System.out::println); // Jasna
+		//List col1 = new ArrayIndexedCollection();
+		//List col2 = new LinkedListIndexedCollection();
+		List col1 = new LinkedListIndexedCollection();
+		List col2 = new ArrayIndexedCollection();
+		
+		col1.add("Ivana");
+		col2.add("Jasna");
+		
+		Collection col3 = col1;
+		Collection col4 = col2;
+		
+		col1.get(0);
+		col2.get(0);
+	
+		//col3.get(0); // neće se prevesti! Razumijete li zašto? 
+		//col4.get(0); // neće se prevesti! Razumijete li zašto?
+		// Class Collection nema metodu get!
+	
+		col1.forEach(System.out::println); // Ivana
+		col2.forEach(System.out::println); // Jasna
+		col3.forEach(System.out::println); // Ivana
+		col4.forEach(System.out::println); // Jasna
 		
 		
 		
